808c5e8c57109e62b0479f3bbc4892206f215265
SAW <99191294+nicsaw@users.noreply.github.com>
2024-09-11 18:50:55 +1000

    Update Banner

--- next-app/src/components/Banner.jsx
+++ next-app/src/components/Banner.jsx
@@ -4,6 +4,9 @@ export default function Banner() {
   return (
     <div className="flex flex-col mt-24 px-8 min-h-screen">
       <h1 className="text-5xl font-bold">Hi! I'm Nicholas</h1>
+      <p>
+        Computer Science student at UNSW specialising in Security Engineering
+      </p>
     </div>
   );
 }
