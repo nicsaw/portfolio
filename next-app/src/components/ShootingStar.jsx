83cfdc9b66450e148308b63d0b25d9dcd009b8cc
SAW <99191294+nicsaw@users.noreply.github.com>
2024-09-11 18:17:01 +1000

    Use useState and useEffect for ShootingStar

--- next-app/src/components/ShootingStar.jsx
+++ next-app/src/components/ShootingStar.jsx
@@ -1,21 +1,27 @@
 import Star from './Star';
 import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
 
 const starSize = 50;
 
 const getRandom = (min, max) => Math.random() * (max - min) + min;
 
 export default function ShootingStar() {
-  const initialX = getRandom(0, window.innerWidth);
-  const finalX = getRandom(0, window.innerWidth);
+  const [initialX, setInitialX] = useState(0);
+  const [finalX, setFinalX] = useState(0);
+
+  useEffect(() => {
+    setInitialX(getRandom(0, window.innerWidth));
+    setFinalX(getRandom(0, window.innerWidth));
+  }, []);
 
   return (
     <motion.div
-      className="fixed"
-      initial={{ x: initialX, y: -100 }}
+      className="fixed z-0"
+      initial={{ x: initialX, y: -starSize * 2 }}
       animate={{
         x: finalX,
-        y: '100vh',
+        y: window.innerHeight + starSize * 2,
         rotate: initialX <= finalX ? 360 : -360,
       }}
       transition={{ duration: 2, ease: 'linear' }}
